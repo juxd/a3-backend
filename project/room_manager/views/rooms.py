c97c72d47293f8e544c15e4a664d08fdc3fd5c26
Liang Jun <au.liangjun@gmail.com>
2019-09-24 16:17:17 +0800

    Fix permissions bug with rooms endpoint

--- project/room_manager/views/rooms.py
+++ project/room_manager/views/rooms.py
@@ -2,7 +2,7 @@ from ..models import Room
 from ..models.room import RoomSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class RoomViewSet(viewsets.ModelViewSet):
     """
@@ -31,7 +31,7 @@ class RoomViewSet(viewsets.ModelViewSet):
         method would require the user to be authenticated.
         """
         if self.action == 'list':
-            permission_classes = []
+            permission_classes = [AllowAny]
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
