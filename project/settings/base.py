83c13157c19f9dc19baa3a57a9c142ad2a8ec31a
Liang Jun <au.liangjun@gmail.com>
2019-09-26 22:10:56 +0800

    Fix redis config for dev

--- project/settings/base.py
+++ project/settings/base.py
@@ -114,7 +114,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-             "hosts": [os.environ.get('REDIS_URL', 'redis://redis:6379')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')],
         },
     },
 }
